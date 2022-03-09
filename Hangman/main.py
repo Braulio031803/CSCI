b2ea5080683a9fad2ae4f80566a705480df04e1a
Braulio031803 <braulio.alfarovazquez01@utrgv.edu>
2022-03-09 10:26:31 -0600

    Initial Commit

---
+++ Hangman/main.py
@@ -0,0 +1,28 @@
+
+import 
+
+# get answer .__doc__
+answer_characters = list("A", "N", "S", "W", "E", "R")
+# store known or unknown letters.
+answer_guesses = lsit()
+
+# Determone which characters are known before strarting the game
+for current_answer_character in answer_characters:
+  if re.search("^[A-Z]")
+
+
+
+
+
+
+  num_of_incorrect_guessess = 0
+
+while num_of_incorrect_guessess < 5 or False in answer_guesses:
+  print("-----------")
+print("Guessed Letters: ", sep = "")
+
+
+print(f"Number of incorrect guessess remaining: {5 - num_of_incorrect_guessess}"")
+
+Print()
+
